805b084c7ec2c30bce919ac00ce1bb0dfcd129f6
Mrunal Ingale <68011977+Mrunal-22@users.noreply.github.com>
2023-01-19 15:31:35 +0530

    Add files via upload

---
+++ Basic Maths/1-ExtractNos.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n > 0)
+    {
+        int lastdigit= n%10;
+        cout << lastdigit;
+        n = n / 10;
+    }
+
+return 0;
+}
